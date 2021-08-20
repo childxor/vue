37778b2c783ce49672b431d0e454cd184683d484
Phongphan Suwantaemee <phongphan_s@eft.domain>
2021-08-20 09:10:38 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <h1 v-if="msg != 10 ? false : true">{{ msg }}</h1>
+    <h2>{{ msg }}</h2>
+    <h2>{{ as }}</h2>
+    <!-- <button v-on:click="Plus()">click</button> -->
+    <input v-model="msg" /> <br />
+    <a v-bind:href="msg">123</a>
+    <button @click="goToAbout">GO TO ABOUT</button>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "hello world",
+  data() {
+    return {
+      msg: "",
+      subTitle: "world",
+      as: "goal",
+    };
+  },
+  methods: {
+    Plus: function() {
+      return this.msg;
+    },
+    goToAbout() {
+      this.$router.push({ name: "Profile" });s
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app2" class="about">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+import abc from "@/components/About.vue"; // @ is an alias to /src
+
+@Component({
+  components: {
+    abc,
+  },
+})
+export default class About extends Vue {}
+</script>
